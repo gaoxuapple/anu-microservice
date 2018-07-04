ab02872afab1a9e6a0726f6ad4321056252f9de4
gaoxuapple <1727753076@qq.com>
2018-07-04 15:39:05 +0800

    增加pom文件

--- src/main/java/com/anu/microservice/anumicroservice/AnuMicroserviceApplication.java
+++ src/main/java/com/anu/microservice/anumicroservice/AnuMicroserviceApplication.java
@@ -2,8 +2,14 @@ package com.anu.microservice.anumicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableSwagger2
 public class AnuMicroserviceApplication {
 
 	public static void main(String[] args) {
